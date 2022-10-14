abbf6ceacc043d0306b82b807602dc793b6cc02a
Mark Anthony <mark.sva@hotmail.com>
2022-10-14 13:02:08 -0300

    up

---
+++ manipulacao-de-strings/obtendo-tamanho-de-strings.js
@@ -0,0 +1,23 @@
+// --------------------------------------------------------- // 
+
+let cep = "31742-450";
+
+if (cep.length != 9) {
+    console.log(`O CEP deve ter 9 caracteres!`);
+} else {
+    console.log(`O CEP foi preenchido corretamente!`);
+}
+
+// --------------------------------------------------------- // 
+
+let nome = "Isabela";
+let frase = `${nome} está aprendendo JavaScript`;
+
+let tamanho_nome = nome.length;
+let frase_nome = frase.length;
+
+console.log(tamanho_nome); // 7
+console.log(frase_nome); // 34
+
+
+
